fa76e008fa019e2600a7f4e0b12a183689d6c219
Daniel Bartolini <djb3149@gmail.com>
2022-12-30 14:40:05 -0500

    added classes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
@@ -26,42 +27,53 @@ const questions = [{
     name: 'managerOffice',
 },
 {
-    type: 'list',
-    message: 'ADD ANOTHER: Would you like to add another employee?',
-    choices: ['Engineer', 'Intern', 'None'],
-    name: 'add'
-},
-{
-    type: 'input',
-    message: 'NAME: What is the engineer\'s name?',
-    name: 'engineerName',
-    when: (answers) => answers.add === 'Engineer',
-},
-{
-    type: 'input',
-    message: 'NAME: What is the engineer\'s ID?',
-    name: 'engineerId',
-    when: (answers) => answers.add === 'Engineer',
-},
-{
-    type: 'input',
-    message: 'EMAIL: What is the engineer\'s email?',
-    name: 'engineerEmail',
-    when: (answers) => answers.add === 'Engineer',
-},
-{
-    type: 'input',
-    message: 'GITHUB: What is the engineer\'s github?',
-    name: 'engineerGithub',
-    when: (answers) => answers.add === 'Engineer',
-}]
+    type: 'loop',
+    message: 'Would you like to add another employee?',
+    name: 'employees',
+    questions: [
+        {
+            type: 'list',
+            message: 'What type of employee are you adding?',
+            name: 'role',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            message: 'NAME: What is the employee\'s name?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'ID: What is the employee\'s ID number?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'EMAIL: What is the employee\'s email address?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'GITHUB: What is the employee\'s GitHub username?',
+            name: 'github',
+            when: (employee) => employee.role === 'Engineer'
+        },
+        {
+            type: 'input',
+            message: 'SCHOOL: What is the name of the employee\'s school?',
+            name: 'school',
+            when: (employee) => employee.role === 'Intern'
+        }
+    ]
+}];
 
 function init() {
     inquirer
         .prompt(questions)
         .then((answers) => {
-            // writeToFile(answers);
             console.log('Data saved. Generating team sheet...');
+
+            const manager = new Manager(answers.managerName, answers.)
         })
 }
 
