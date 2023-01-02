1cada2f3ab10763434add6f165a5b6b83b01de93
Daniel Bartolini <djb3149@gmail.com>
2023-01-02 14:47:58 -0500

    added engineer and manager test suites

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,20 @@
 const Engineer = require("../lib/Engineer");
 
-describe("Engineer", () => {
+describe('Engineer', () => {
+    describe('Data', () => {
+        it('should return an object with data for name, id, email, and office when called with the \'new\' keyword', () => {
+            const obj = new Engineer('Engineer', 'Dan', 1, 'dan@email.com', 'dangithub');
 
+            expect('role' in obj && 'name' in obj && 'id' in obj && 'email' in obj).toEqual(true);
+        })
+    })
+    describe('Methods', () => {
+        describe('getGithub', () => {
+            it('should return the github username of the employee', () => {
+                const obj = new Engineer('Engineer', 'Dan', 1, 'dan@email.com', 'dangithub');
+
+                expect(obj.getGithub()).toEqual('dangithub');
+            });
+        })
+    })
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,21 @@
 const Manager = require("../lib/Manager");
 
-describe("Manager", () => {
+describe('Manager', () => {
+    describe('Data', () => {
+        it('should return an object with data for name, id, email, and office when called with the \'new\' keyword', () => {
+            const obj = new Manager('Dan', 1, 'dan@email.com', 123);
 
+            expect('name' in obj && 'id' in obj && 'email' in obj && 'office' in obj).toEqual(true);
+        })
+    })
+    describe('Methods', () => {
+        describe('getRole', () => {
+            it('should return manager', () => {
+                const obj = new Manager();
+
+                expect(obj.getRole()).toEqual('Manager');
+            })
+        })
+    })
 });
 
